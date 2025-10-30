38a2a519e544c12264dff26d7e904b646f275126
Minh <xuanminh1998z@gmail.com>
2025-10-30 15:18:09 +0700

    initial commit

--- products.py
+++ products.py
@@ -58,12 +58,70 @@ if not weaviate_client.collections.exists('products'):
 else:
     products=weaviate_client.collections.get('products')
 
-import PIL.Image as Image
-response=products.query.near_text("""""", limit=3)
-for obj in response.objects:
-    print(obj.properties)
-    print('images/'+str(obj.properties['product_id'])+'.jpg')
-    img=Image.open('images/'+str(obj.properties['product_id'])+'.jpg')
-    img.show()
-print(type(response.objects))
+if not weaviate_client.collections.exists('faqs'):
+    faqs=weaviate_client.collections.create(
+        name='faqs',
+        vector_config=Configure.Vectors.text2vec_weaviate(
+            name='vector',
+            source_properties=['question', 'answer', 'type']),
+        properties=[
+            Property(name='question', vectorize_property_name=False, data_type=DataType.TEXT),
+            Property(name='answer', vectorize_property_name=False, data_type=DataType.TEXT),
+            Property(name='type', vectorize_property_name=False, data_type=DataType.TEXT)
+            ]
+                    
+    )
+    import joblib
+    faq_data=joblib.load('faq.joblib')
+    with faqs.batch.fixed_size(batch_size=100) as batch:
+        for faq in faq_data:
+            batch.add_object(
+                {'question': faq['question'],
+                'answer': faq['answer'],
+                'type': faq['type'],
+                })
+            if batch.number_errors >10:
+                print('Batch import stopped due to too many errors.')
+                break
+else:
+    faqs=weaviate_client.collections.get('faqs')
+
+query='how can i pay'
+
+import os
+from openai import OpenAI
+client=OpenAI(
+    base_url='https://router.huggingface.co/v1',
+    api_key='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')
+completion=client.chat.completions.create(
+    model="MiniMaxAI/MiniMax-M2:novita",
+    messages=[{'role':'user',"content":f"""Label the following instruction as an FAQ-related query or a produt-related query.
+               Return only 'FAQ' or 'Product'.
+               'Query':{query}"""}])
+question_type=completion.choices[0].message.content
+
+response=faqs.query.near_text(query, limit=3)
+
+context=""
+for res in response.objects:
+    context+=f"Question: {res.properties['question']} Answer: {res.properties['answer']}\n"
+print(context)
+
+completion=client.chat.completions.create(
+    model="MiniMaxAI/MiniMax-M2:novita",
+    messages=[{'role':'system',"content":'You are a helpful assistant of an online clothing store'},
+        {'role':'user',"content":f""" Answer the faq question {query} just using the information bellow:
+                {context}.
+                Respond as you are a representative of the clothing store."""}])
+print(completion.choices[0].message.content)
+
+completion=client.chat.completions.create(
+    model="MiniMaxAI/MiniMax-M2:novita",
+    messages=[{'role':'system',"content":'You are a helpful assistant of an online clothing store'},
+        {'role':'user',"content":f""""""}])
+print(completion.choices[0].message.content)
+
+
+
+
 weaviate_client.close()
