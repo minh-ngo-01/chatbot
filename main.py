592c18199729d93c72f7ddeb8d9efdfd31fe384c
Minh <xuanminh1998z@gmail.com>
2025-11-02 15:35:46 +0700

    done mvp

--- main.py
+++ main.py
@@ -1,12 +1,82 @@
+from weaviate_utils import get_products_collection
 from utils import classify_query
 from utils import query_faq
 from utils import query_product
+from fastapi import FastAPI
+from pydantic import BaseModel
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import FileResponse
+import os
 
-query='i want to buy t-shirt for men'
-query_type=classify_query(query)
-print(query_type)
-if query_type=='FAQ':
-    res=query_faq(query)
-elif query_type=='Product':
-    res=query_product(query)
-print(res)
+app=FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['*'],
+    allow_credentials=True,
+    allow_methods=['*'],
+    allow_headers=['*'],
+)
+
+app.mount("/static", StaticFiles(directory='frontend'), name='static')
+@app.get('/')
+def serve_index():
+    return FileResponse(os.path.join('frontend', 'index.html'))
+
+@app.get('/add_product_page')
+def serve_add_product_page():
+    return FileResponse(os.path.join('frontend', 'input.html'))
+
+
+class Question(BaseModel):
+    query: str
+class Product(BaseModel):
+    gender: str
+    masterCategory: str
+    subCategory: str
+    articleType: str
+    baseColour: str
+    season: str
+    year: int
+    usage: str
+    productDisplayName: str
+    price: int
+    product_id: int
+
+@app.post('/add_product')
+def add_product(product: Product):
+    collection=get_products_collection()
+    try:
+        collection.data.insert(
+            properties={
+            'gender': product.gender,
+            'masterCategory': product.masterCategory,
+            'subCategory': product.subCategory,
+            'articleType': product.articleType,
+            'baseColour': product.baseColour,
+            'season': product.season,
+            'year': product.year,
+            'usage': product.usage,
+            'productDisplayName': product.productDisplayName,
+            'price': product.price,
+            'product_id': product.product_id 
+            })
+        return {'message': f'Product {product.product_id} added'}
+    except Exception as e:
+        return {'error': str(e)}
+        
+
+@app.post('/chat')
+def answer(question: Question):
+    query=question.query
+    query_type=classify_query(query)
+    if query_type=='FAQ':
+        res=query_faq(query)
+    elif query_type=='Product':
+        res=query_product(query)
+    return {'message':res}
+
+@app.get('/')
+def root():
+    return {'message':'FastAPI chatbot backend running!'}
+
