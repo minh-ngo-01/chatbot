0a4ed509b5ff0aeed496b968167ae133e8a6f5d9
Minh <xuanminh1998z@gmail.com>
2025-11-28 12:01:01 +0700

    remote

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 import re
 import json
 text="""{"reasoning": "Khách hàng đang tìm kiếm"}"""
-print(re.search(r'{\"(.+)\"}',text))
-print(re.search(r'{\"(.+)\"}',text).group(1))
+print(re.sub(r'{\"(.+)\"}',))
+# print(re.search(r'{\"(.+)\"}',text).group(1))
 # match=re.search(r'\"reasoning\":\\s\"(.+)\", \"response\"', text,flags=re.DOTALL)
 # print(match.group(0))
